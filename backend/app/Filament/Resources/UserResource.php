5d03003c13530abcffbeb2e1f0d598f9cd9dc7c0
neville03 <akoneville1@gmail.com>
2026-08-04 23:17:35 +0300

    fix(admin): remove duplicate shouldRegisterNavigation on UserResource

--- backend/app/Filament/Resources/UserResource.php
+++ backend/app/Filament/Resources/UserResource.php
@@ -34,6 +34,8 @@ class UserResource extends Resource
 
     protected static ?int $navigationSort = 1;
 
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
